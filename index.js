7e11def7bab4cc6b682823669a43d561333fdf32
haben mehar <habenmehari71@gmail.com>
2023-06-14 10:37:41 +0300

    linter problems fixed and luxon added to the project

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+import {DateTime} from './node_modules/luxon/src/luxon.js';
 import Book from './modules/bookClass.js';
 import { resetForm } from './modules/utils.js';
 
 const form = document.getElementById('form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
+const timeDate = document.getElementById('time-date');
 
 const newBook = new Book();
+
+timeDate.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+
+document.getElementById('book-list').scrollIntoView({
+  behavior: 'smooth',
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
